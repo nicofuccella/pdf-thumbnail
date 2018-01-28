45f3f68410f16ea8558d5c96e1b042fe365a1ab1
Ng2k <nicola.ng2k@gmail.com>
2018-01-28 22:40:29 +0100

    mods

--- test/test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const preview = require('../index');
-
-preview(fs.readFileSync('./IL SETTECENTO POLITICO inglese.pdf'))
-  .then(data => fs.writeFileSync('preview.jpg', data))
-  .catch(err => console.error(err));
