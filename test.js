62cb218fa982144f8e1bab3325f35f8bf76e3daa
Ng2k <nicola.ng2k@gmail.com>
2018-02-04 17:55:26 +0100

    mods

--- test.js
+++ test.js
@@ -3,15 +3,6 @@
 const fs = require('fs');
 const preview = require('./index');
 
-preview(fs.createReadStream('./test/KWB UTENSILI MANUALI.pdf'), {
-  resize: {
-    width: 300,
-    height: 400
-  },
-  compress: {
-    quality: 100,
-    type: 'jpeg'
-  }
-})
+preview()
   .then(data => fs.writeFileSync('./test/preview.jpg', data))
   .catch(err => console.error(err));
